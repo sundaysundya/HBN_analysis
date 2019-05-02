d2a3843116f267599ffe5ec3fcf2ac661c4484ac
Sandhya Ramachandran <sandhya@omicron>
2019-05-02 14:31:28 -0400

    added pheno vs pheno test

--- code/idcompare.py
+++ code/idcompare.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb  8 15:19:31 2019
-
+This code finds the intersection of connectome data and phenotypic data for one test at a time, for each atlas.
 @author: sandhya
 """
 
@@ -10,92 +10,98 @@ import scipy.io as scp
 import pandas as pd
 import os
 
-atlas = 'aal'
-path = '/cis/home/sandhya/NDD/edgelists_fmri' + atlas
-files = [f for f in os.listdir(path) if f.endswith('.edgelist')]
-
-names = [None] * len(files)
-
-
-for i in range(0,len(files)):
-    names[i] = files[i][4:16]
-
-
-X = np.load('Xdata_'+atlas+'.npy')
-
-ASSQ = pd.ExcelFile('aut_pheno/ASSQ.xlsx')
-ASSQ = ASSQ.parse('Sheet1')
-ASSQ = ASSQ.values
-assqnam=ASSQ[:,0].astype('str')
-for i in range(0, len(assqnam)):
-    assqnam[i] = assqnam[i][1:13]
-assqnam2 = assqnam.reshape(1401,1)
-assqnam= np.ndarray.tolist(assqnam)
-assqdat = ASSQ[:,1].astype('int').reshape(1401,1)
-assqtab = np.concatenate((assqnam2, assqdat), axis=1)
-
-
-SCQ = pd.ExcelFile('aut_pheno/SCQ.xlsx')
-SCQ = SCQ.parse('Sheet1')
-SCQ = SCQ.values
-scqnam=SCQ[:,0].astype('str')
-for i in range(0, len(scqnam)):
-    scqnam[i] = scqnam[i][1:13]
-scqnam2 = scqnam.reshape(1406,1)
-scqnam= np.ndarray.tolist(scqnam)
-scqdat = SCQ[:,1].astype('int').reshape(1406,1)
-scqtab = np.concatenate((scqnam2, scqdat), axis=1)
-
-
-SRS = pd.ExcelFile('aut_pheno/SRS.xlsx')
-SRS = SRS.parse('Sheet1')
-SRS = SRS.values
-srsnam=SRS[:,0].astype('str')
-for i in range(0, len(srsnam)):
-    srsnam[i] = srsnam[i][1:13]
-srsnam2 = srsnam.reshape(1303,1)
-srsnam= np.ndarray.tolist(srsnam)
-srsdat = SRS[:,1].astype('int').reshape(1303,1)
-srstab = np.concatenate((srsnam2, srsdat), axis=1)
-
-
-SRSPre = pd.ExcelFile('aut_pheno/SRSPre.xlsx')
-SRSPre = SRSPre.parse('Sheet1')
-SRSPre = SRSPre.values
-srsprenam=SRSPre[:,0].astype('str')
-srsprenam= np.ndarray.tolist(srsprenam)
-for i in range(0, len(srsprenam)):
-    srsprenam[i] = srsprenam[i][1:13]
+        #find intersection of correlation matrix data and phenotypic data subjects
+atlases = ('JHU','aal','brodmann','CPAC200','desikan','DK','HarOxCort','HarOxSub','hemispheric','pp264','tissue')
+for atlas in atlases:
+    path = '/cis/home/sandhya/NDD/edgelists_fmri' + atlas
+    files = [f for f in os.listdir(path) if f.endswith('.edgelist')]
+    
+    names = [None] * len(files)
+    
+    
+    for i in range(0,len(files)):
+        names[i] = files[i][4:16]
+    
+    
+    X = np.load('Xdata_'+atlas+'.npy')
+    
+    
+                #single test at a time
+    testname = 'ACE'
+    test = pd.ExcelFile('cleaned_pheno/clean_'+testname+'.xlsx') #'aut_pheno/ASSQ.xlsx'
+    test = test.parse('Sheet1')
+    test = test.values
+    testnam=test[:,0].astype('str')
+    #for i in range(0, len(assqnam)):
+    #    assqnam[i] = assqnam[i][1:13]
+    sz = testnam.size
+    testnam2 = testnam.reshape(sz,1)
+    testnam= np.ndarray.tolist(testnam)
+    testdat = test[:,1].astype('int').reshape(sz,1)
+    testtab = np.concatenate((testnam2, testdat), axis=1)
+    
+    intersection = list(set(names) & set(testnam))
+    
+    
+    
+    Yintersect = np.zeros([len(intersection),1])
+    for i in range(0,len(intersection)):
+        for j in range(0,len(testtab)):
+            if intersection[i]==testtab[j,0]:
+                Yintersect[i,0] = testtab[j,1]
+    
+    names2 = np.array(names).reshape(len(files),1)
+    Xdata = np.concatenate((names2,X), axis=1)
+    
+    Xintersect = np.zeros([len(intersection),X.shape[1]])
+    for i in range(0, len(intersection)):
+        for j in range(0,len(Xdata)):
+            if intersection[i] == Xdata[j,0]:
+                Xintersect[i,:]=Xdata[j,1:(X.shape[1]+1)]
+                
+    np.save('Xintersect_c_'+atlas,Xintersect)
+    np.save('Yintersect_c_'+atlas,Yintersect)
 
 
-subjectsASSQ = list(set(names) & set(assqnam))
-subjectsSCQ = list(set(names) & set(scqnam))
-subjectsSRS = list(set(names) & set(srsnam))
-subjectsSRSPre = list(set(names) & set(srsprenam))
 
-intersection = list(set(subjectsASSQ) & set(subjectsSCQ) & set(subjectsSRS))
+        #find intersection of two phenotypic tests
+testname = 'ARI_P'
+test = pd.ExcelFile('cleaned_pheno/clean_'+testname+'.xlsx')
+test = test.parse('Sheet1')
+test = test.values
+testnam=test[:,0].astype('str')
+sz = testnam.size
+testnamb = testnam.reshape(sz,1)
+testnam= np.ndarray.tolist(testnam)
+testdat = test[:,1].astype('int').reshape(sz,1)
+testtab = np.concatenate((testnamb, testdat), axis=1)
 
+testname2 = 'ARI_S'
+test2 = pd.ExcelFile('cleaned_pheno/clean_'+testname2+'.xlsx')
+test2 = test2.parse('Sheet1')
+test2 = test2.values
+testnam2=test2[:,0].astype('str')
+#for i in range(0, len(assqnam)):
+#    assqnam[i] = assqnam[i][1:13]
+sz = testnam2.size
+testnamb2 = testnam2.reshape(sz,1)
+testnam2= np.ndarray.tolist(testnam2)
+testdat2 = test2[:,1].astype('int').reshape(sz,1)
+testtab2 = np.concatenate((testnamb2, testdat2), axis=1)
 
-Yintersect = np.zeros([len(intersection),3])
+intersection = list(set(testnam) & set(testnam2))
+
+Yintersect = np.zeros([len(intersection),1])
 for i in range(0,len(intersection)):
-    for j in range(0,len(assqtab)):
-        if intersection[i]==assqtab[j,0]:
-            Yintersect[i,0] = assqtab[j,1]
-    for j in range(0,len(scqtab)):
-        if intersection[i]==scqtab[j,0]:
-            Yintersect[i,1] = scqtab[j,1]
-    for j in range(0,len(srstab)):
-        if intersection[i]==srstab[j,0]:
-            Yintersect[i,2] = srstab[j,1]
+    for j in range(0,len(testtab)):
+        if intersection[i]==testtab[j,0]:
+            Yintersect[i,0] = testtab[j,1]
 
-names2 = np.array(names).reshape(len(files),1)
-Xdata = np.concatenate((names2,X), axis=1)
-
-Xintersect = np.zeros([len(intersection),X.shape[1]])
-for i in range(0, len(intersection)):
-    for j in range(0,len(Xdata)):
-        if intersection[i] == Xdata[j,0]:
-            Xintersect[i,:]=Xdata[j,1:(X.shape[1]+1)]
+Xintersect = np.zeros([len(intersection),1])
+for i in range(0,len(intersection)):
+    for j in range(0,len(testtab2)):
+        if intersection[i]==testtab2[j,0]:
+            Xintersect[i,0] = testtab2[j,1]
             
-np.save('Xintersect_'+atlas,Xintersect)
-np.save('Yintersect_'+atlas,Yintersect)
+np.save('Xintersect_'+testname,Xintersect)
+np.save('Yintersect_'+testname2,Yintersect)
